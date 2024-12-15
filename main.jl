16ead7e21dd8d56b125fddac32607296ea56378d
Noah Syrkis <noah@syrkis.com>
2024-12-15 17:22:59 +0100

    repl repl repl

--- main.jl
+++ main.jl
@@ -5,6 +5,8 @@ function main()
     println("Fetching Wikipedia pageview data...")
     data = data_fn()
     println("Data fetching complete!")
+    # Unique articles in the US dataset
+    # map(x -> map(y -> y.article, x.articles) |> Set, values(data["US"])) |> x -> reduce(union, x)
     return data
 end
 
